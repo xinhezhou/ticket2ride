d5eb070b89f48e3f19572bfa7997be90e02d5aa6
Midori <xmzhou@mit.edu>
2022-03-10 15:44:53 -0500

    loss file

--- dual/cnn/rl/selfplay.py
+++ dual/cnn/rl/selfplay.py
@@ -10,17 +10,18 @@ from sl.cnn import train_cnn, push_memory
 from evaluation.evaluation import evaluate_net
 from exprience_replay import ReplayMemory
 
-def train_selfplay(initial_checkpoint, selfplay_checkpoint, record_file, loss_file, memory, player_classes, destination_cards, opponent_net=None, first=True, round=10):
+def train_selfplay(initial_checkpoint, selfplay_checkpoint, record_file, memory_file, loss_file, memory, player_classes, destination_cards, opponent_net=None, first=True, round=1):
     policy_net = load_net(initial_checkpoint)
     target_net = load_net(initial_checkpoint, eval=True)
     for _ in range(round):
+        target_nets = [target_net, load_net(opponent_net)]
+        if not first:
+            target_nets = target_nets[:]
+        evaluate_net(target_nets, player_classes, destination_cards,record_file, counter=100, display=False)
+        convert_records(record_file, memory_file)
+        push_memory(memory_file, memory)
         train_cnn(policy_net, target_net, memory, selfplay_checkpoint, loss_file)
-
-    target_nets = [target_net, load_net(opponent_net)]
-    if not first:
-        target_nets = target_nets[:]
-    evaluate_net(target_nets, player_classes, destination_cards,record_file, counter=100, display=False)
-
+        
 
 
 """
@@ -41,9 +42,4 @@ destination_cards = [
     [[1, 3], [1, 6]],
     [[0, 6], [3, 6]],
 ]
-train_selfplay(initial_checkpoint, selfplay_checkpoint, record_file, loss_file, memory, player_classes, destination_cards)
-for i in range(100):
-    train_selfplay(selfplay_checkpoint, selfplay_checkpoint, record_file, memory, player_classes, destination_cards)
-    convert_records(record_file, memory_file)
-    push_memory(memory_file, memory)
-
+train_selfplay(initial_checkpoint, selfplay_checkpoint, record_file, memory_file, loss_file, memory, player_classes, destination_cards)
